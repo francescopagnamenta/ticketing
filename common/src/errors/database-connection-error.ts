d8809eb581acc50bb4cecca4294ee8beb0530c79
Francesco Pagnamenta <fpagnamenta@gmail.com>
2021-04-18 18:23:39 +0200

    commit module initial commit

---
+++ common/src/errors/database-connection-error.ts
@@ -0,0 +1,20 @@
+import { CustomError } from './custom-error';
+
+export class DatabaseConnectionError extends CustomError {
+
+  statusCode = 500;
+  reason = 'Error connecting to database';
+
+  constructor() {
+    super('Database error');
+
+      // Only because we are extending a build in class
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      {message: this.reason}
+    ]
+  }
+}
